e763711e9fdc036def4b30b6660a735d057860a1
Vi <silvia.copil@gmail.com>
2022-08-13 15:36:35 +0200

    preparing port to PyPortal

--- MineSweeper on PyPortal/board.py
+++ MineSweeper on PyPortal/board.py
@@ -1,5 +1,4 @@
-import random 
-import re
+import random
 
 class Board:
     def __init__(self, dim_size, num_bombs):
@@ -124,40 +123,4 @@ class Board:
         string_rep = indices_row + '-'*str_len + '\n' + string_rep + '-'*str_len
 
         return string_rep
-
-
-def play(dim_size = 10, num_bombs = 10):
-
-    board = Board(dim_size, num_bombs)
-
-    safe = True
-    while len(board.dug) < (board.dim_size ** 2 - num_bombs):
-        print(board)
-        user_input = re.split(",(\\s)*", input("Where would you like to dig? Input as row, col ") ) 
-        # pattern to split the input by the comma, and ignore white spaces
-        row, col = int(user_input[0]), int(user_input[-1]) # take first and last elements, to avoid split extras (?)
-
-        if row < 0 or row >= board.dim_size or col < 0 or col >= board.dim_size:
-            print("Invalid location, try again.")
-            continue
-
-        safe = board.dig(row, col)
-
-        if not safe:
-            # YOU. LOST. GAME. OVER
-            break
-    
-    if safe: 
-        print("You won! YAY!!!")
-        print(board)
-    else:
-        print("GAME. OVER")
-        board.dug = [(r,c) for r in range(board.dim_size) for c in range(board.dim_size)]
-        print(board)
-
-
-
-if __name__ == '__main__': 
-    play()
-    
 
---
+++ MineSweeper on PyPortal/minesweeper_PC.py
@@ -0,0 +1,44 @@
+import random 
+import re
+from board import Board
+
+def play(dim_size = 10, num_bombs = 10):
+
+    board = Board(dim_size, num_bombs)
+
+    safe = True
+    while len(board.dug) < (board.dim_size ** 2 - num_bombs):
+        print(board)
+        user_input = re.split(",(\\s)*", input("Where would you like to dig? Input as row, col ") ) 
+        # pattern to split the input by the comma, and ignore white spaces
+
+        try:
+            row, col = int(user_input[0]), int(user_input[-1]) # take first and last elements, to avoid split extras (?)
+        except ValueError:
+            print("Invalid format, try again.")
+            continue
+        else:   
+            if row < 0 or row >= board.dim_size or col < 0 or col >= board.dim_size:
+                print("Invalid location, try again.")
+                continue
+
+        safe = board.dig(row, col)
+
+        if not safe:
+            # YOU. LOST. GAME. OVER
+            break
+    
+    if safe: 
+        print("You won! YAY!!!")
+        print(board)
+    else:
+        print("GAME. OVER")
+        board.dug = [(r,c) for r in range(board.dim_size) for c in range(board.dim_size)]
+        print(board)
+
+
+
+if __name__ == '__main__': 
+    play()
+    
+
