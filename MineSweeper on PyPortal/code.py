d1f08d32f81f96802b5cff4e1bf58c46349cda9f
Snakey Maker Cat =^.^= <silvia.copil@gmail.com>
2022-08-27 22:38:11 +0200

    Update code.py

--- MineSweeper on PyPortal/code.py
+++ MineSweeper on PyPortal/code.py
@@ -1,8 +1,10 @@
+# TODO main menu
+
 import time
 import supervisor
 
 while True:
     print("loading minesweeper...")
-    time.sleep(2)
+    # time.sleep(2)
     supervisor.set_next_code_file('minesweeper.py')
     supervisor.reload()
